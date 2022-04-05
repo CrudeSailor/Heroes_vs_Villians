bbaa8e184ce82bb66a4fed0001d9254bfdf8053f
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-04-05 13:39:49 -0700

    Changed database settings

--- heroes_vs_villians_project/settings.py
+++ heroes_vs_villians_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z7e8)0q=q$h(yb)k8qy9b)2-o&-e5p3v9pr(s0mjz&pd*aw4dm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_vs_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +110,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from heroes_vs_villians_project.local_settings import *
+except ImportError:
+    pass    
